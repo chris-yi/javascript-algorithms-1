5204a43aa1d88fb9e32b865cc10e91856854c70e
chris-yi <kri5to.yi@gmail.com>
2020-04-06 16:52:23 -0700

    linkedlist

---
+++ src/data-structures/linked-list/LinkedList-practice.js
@@ -0,0 +1,101 @@
+import LinkedListNode from './LinkedListNode-practice';
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.next);
+
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  // delete(value) {
+  //   if(!this.head) {
+  //     return null;
+  //   }
+
+  //   let deletedNode = null;
+
+  //   while(this.head && ``)
+  // }
+
+  find(value = undefined) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  deleteTail() {
+    const deletedTail = this.tail;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    this.tail = currentNode;
+    return deletedTail;
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+}
