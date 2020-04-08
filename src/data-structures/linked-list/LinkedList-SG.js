80ad0bb926b719d8eecd1e4bd9230849286b4913
chris-yi <kri5to.yi@gmail.com>
2020-04-07 19:39:43 -0700

    LL push pop shift unshift

---
+++ src/data-structures/linked-list/LinkedList-SG.js
@@ -0,0 +1,81 @@
+import Node from './LinkedListNode-SG';
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  pop(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      return undefined;
+    }
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let currentHead = this.head;
+
+    this.head = currentHead.next;
+    this.length--;
+
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.count++;
+    return this;
+  }
+}
