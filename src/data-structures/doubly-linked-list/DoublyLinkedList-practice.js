c6e8433fd2653fbcedf47b0516bb359b7801ded2
chris-yi <kri5to.yi@gmail.com>
2020-04-16 20:36:06 -0700

    doubly LL, push & pop

--- src/data-structures/doubly-linked-list/DoublyLinkedList-practice.js
+++ src/data-structures/doubly-linked-list/DoublyLinkedList-practice.js
@@ -4,37 +4,75 @@ export default class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.count = 0;
   }
 
-  prepend(value) {
-    const newNode = new DoublyLinkedListNode(value, this.head);
+  // prepend(value) {
+  //   const newNode = new DoublyLinkedListNode(value, this.head);
 
-    if (this.head) {
-      this.head.prev = newNode;
-    }
-    this.head = newNode;
+  //   if (this.head) {
+  //     this.head.prev = newNode;
+  //   }
+  //   this.head = newNode;
 
-    if (!this.tail) {
-      this.tail = newNode;
-    }
+  //   if (!this.tail) {
+  //     this.tail = newNode;
+  //   }
 
-    return this;
-  }
+  //   return this;
+  // }
 
-  append(value) {
-    const newNode = new DoublyLinkedListNode(value);
+  // append(value) {
+  //   const newNode = new DoublyLinkedListNode(value);
+
+  //   if (!this.head) {
+  //     this.head = newNode;
+  //     this.tail = newNode;
+
+  //     return this;
+  //   }
+
+  //   this.tail.next = newNode;
+  //   newNode.previous = this.tail;
+  //   this.tail = newNode;
+
+  //   return this;
+  // }
+
+  push(val) {
+    const newNode = new DoublyLinkedListNode(val);
 
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-
+      this.count += 1;
       return this;
     }
 
     this.tail.next = newNode;
-    newNode.previous = this.tail;
+    newNode.prev = this.tail;
     this.tail = newNode;
-
+    this.count += 1;
     return this;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    const removedTail = this.tail;
+
+    if (this.count === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedTail.prev;
+      this.tail.next = null;
+      removedTail.prev = null;
+    }
+
+    this.count -= 1;
+    return removedTail;
   }
 }
